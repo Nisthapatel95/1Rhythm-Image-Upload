55480190ba8152e4124ea7aa551331297508c744
Nisthapatel95 <nisthapatel95@gmail.com>
2022-11-18 14:42:30 +0530

    feat : drag and drop with validation

--- src/app/image-upload/image-upload.component.ts
+++ src/app/image-upload/image-upload.component.ts
@@ -10,12 +10,18 @@ export class ImageUploadComponent implements OnInit {
 
   files: File[] = [];
   public total_images: number = 0;
+  selectedImage: any;
   constructor(private notifyService: NotificationService) { }
 
   ngOnInit(): void {
   }
 
   onSelect(event: any) {
+    console.log(event);
+    let beforeUpdateFile = this.files.length;
+    if(event.rejectedFiles.length!=0){
+      this.notifyService.showError("Please upload a valid file format (Supported file formats: .jpg, .png, .jpeg, .heif)");
+    }
     this.total_images += event.addedFiles.length;
     if (this.files.length > 5 || this.total_images > 5) {
       this.notifyService.showError("You Can Only Select Upto 5 Images !");
@@ -29,16 +35,18 @@ export class ImageUploadComponent implements OnInit {
       for (var i = 0; i < this.files.length; i++) {
         formData.append("file[]", this.files[i]);
       }
-      this.notifyService.showSuccess("Image Uploaded Successfully");
     }
+    let afterUpdateFile = this.files.length;
+    (beforeUpdateFile != afterUpdateFile) && this.notifyService.showSuccess("Image Uploaded Successfully");
   }
   onRemove(event: any) {
     console.log(event);
     this.files.splice(this.files.indexOf(event), 1);
     this.total_images = this.total_images - 1;
     console.log(this.total_images);
+  }
 
-   
-
+  setFavImage(selectedImage: any) {
+    this.selectedImage = selectedImage
   }
 }
